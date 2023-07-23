474ba2a4c3c42960b10ae1ff1d6cbd8036b8ed03
Tony Chow <tonychow.0929@icloud.com>
2023-07-23 16:55:23 +0800

    Database migration for share link table

---
+++ server/schemas/migrations/20230723085517-create-share-link-table.cjs
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+    async up (queryInterface, Sequelize) {
+        await queryInterface.sequelize.query(
+            `CREATE TABLE image_share_links (
+                 token TEXT PRIMARY KEY,
+                  image_id UUID NOT NULL REFERENCES user_images(id) ON DELETE CASCADE,
+                 total_limit INTEGER NOT NULL DEFAULT 1,
+                 used_limit INTEGER NOT NULL DEFAULT 0,
+                 CHECK (total_limit > 0),
+                 CHECK (total_limit >= used_limit)
+             )`
+        );
+        await queryInterface.sequelize.query(
+            `CREATE INDEX image_share_links_image_id_btree
+             ON image_share_links
+             USING BTREE(image_id)`,
+        );
+    },
+
+    async down (queryInterface, Sequelize) {
+        await queryInterface.sequelize.query(
+            `DROP TABLE IF EXISTS image_share_links`
+        );
+    },
+};
