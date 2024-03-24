f9511d1e49beb3ec5795ecd7db3070a83805d22e
Tony Chow <tonychow.0929@icloud.com>
2024-03-24 20:24:30 +0800

    Remove unused about page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -49,14 +49,9 @@ const router = createRouter({
                 },
             }),
         },
-        {
-            path: "/about",
-            name: "about",
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import("../views/AboutView.vue"),
-        },
+        // route level code-splitting
+        // this generates a separate chunk (About.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
     ],
 });
 
